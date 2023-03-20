eaa83ba3312fdb2e08797a44a494dd782479cf1a
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-20 20:10:23 -0300

    aula_20_03_2023

--- lista.py
+++ lista.py
@@ -4,3 +4,10 @@ os.system("cls") #Limpa o terminal
 lista1 = ["Informática", "Medicina", "Engenharia"]
 
 print(type(lista1))
+
+lista1.append("Bomedicina") #para inserir um valor
+
+lista1.remove("Informática")
+
+for litageral in lista1:
+    print(litageral)
