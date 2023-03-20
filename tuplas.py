c3f2133210a0b32e41272cbb3b4e2219b4de117d
JoaoPNuneSilva <joaopedronunesdasilva2@gmail.com>
2023-03-20 20:01:29 -0300

    aula_20_03_2023

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+carros = ("Fusca", "Verona", "Gol")
+import os
+
+os.system("cls") #Limpa o terminal
+print(type(carros))
+
+for tuplas in carros:
+    print(tuplas)
+    print("---------------------------------")
